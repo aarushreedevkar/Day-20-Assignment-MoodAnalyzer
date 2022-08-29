2408e0988cfb93ea4b960213649a54d1bde7a176
jyoti <jyotidhage10@gmail.com>
2022-08-29 16:09:24 +0530

    [Aaradhya]Add:Mood Analyser Problem 4 Solution

---
+++ MoodAnalayzerProblems1/MoodAnalyserException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalayzerProblems1
+{
+    
+    
+        public class MoodAnalyserException : Exception
+        {
+            /// <summary>
+            /// Enum of exception type.
+            /// </summary>
+            /// creating type variable of type ExceptionType
+            ExceptionType type;
+            public enum ExceptionType
+            {
+                NULL_EXCEPTION, EMPTY_EXCEPTION, NO_SUCH_FIELD, NO_SUCH_METHOD, NO_SUCH_CLASS, OBJECT_CREATION_ISSUE
+            }
+            /// <summary>
+            /// parameterized contructor sets the Exception Type and message.
+            /// </summary>
+            /// <param name="type"></param>
+            /// <param name="message"></param>
+            public MoodAnalyserException(ExceptionType type, string message) : base(message)
+            {
+                this.type = type;
+            }
+        }
+    }
+
+
+
